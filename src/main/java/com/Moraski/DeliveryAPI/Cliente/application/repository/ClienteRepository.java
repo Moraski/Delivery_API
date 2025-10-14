21a147b376ecdda5c7a3702f881ff030613e119d
Felipe Moraski <moraski060@gmail.com>
2025-10-14 16:31:29 -0300

    feat: Criação do método salva em ClienteRepository e em ClienteRepository.mySQL

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/application/repository/ClienteRepository.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/repository/ClienteRepository.java
@@ -1,4 +1,7 @@
 package com.Moraski.DeliveryAPI.Cliente.application.repository;
 
+import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
+
 public interface ClienteRepository {
+    Cliente salva(Cliente cliente);
 }
--- src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteRepositoryMySQL.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteRepositoryMySQL.java
@@ -1,6 +1,23 @@
 package com.Moraski.DeliveryAPI.Cliente.infra;
 
 import com.Moraski.DeliveryAPI.Cliente.application.repository.ClienteRepository;
+import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
+@Slf4j
 public class ClienteRepositoryMySQL implements ClienteRepository {
+
+    private final ClienteMySQLSpringRepository clienteMySQLSpringRepository;
+
+    @Override
+    public Cliente salva(Cliente cliente) {
+        log.info("[Inicia] ClienteRepositoryMySQL - salva");
+        Cliente novoCliente = clienteMySQLSpringRepository.save(cliente);
+        log.info("[Finaliza] ClienteRepositoryMySQL - salva");
+        return novoCliente;
+    }
 }
