be2f034e0b5446a711cf7d47a6ce05b20b46de91
Felipe Moraski <moraski060@gmail.com>
2025-10-14 16:57:22 -0300

    feat: Criação de  um método para verificar de o email do novo Usuario já existe

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/application/service/ClienteApplicationService.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/service/ClienteApplicationService.java
@@ -4,8 +4,10 @@ import com.Moraski.DeliveryAPI.Cliente.application.api.ClienteNovoRequest;
 import com.Moraski.DeliveryAPI.Cliente.application.api.ClienteResponse;
 import com.Moraski.DeliveryAPI.Cliente.application.repository.ClienteRepository;
 import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
+import com.Moraski.DeliveryAPI.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -18,6 +20,13 @@ public class ClienteApplicationService implements ClienteService{
     @Override
     public ClienteResponse criarNovoCliente(ClienteNovoRequest clienteNovo) {
         log.info("[Inicia] ClienteApplicationService - criaNovoCliente");
+
+        boolean verificaEmail = clienteRepository.buscaPorEmail(clienteNovo.getEmail()).isPresent();
+        if (verificaEmail) {
+            log.warn("[Falha] Tentativa de cadastro com e-mail já existente: {}", clienteNovo.getEmail());
+            throw APIException.build(HttpStatus.CONFLICT, "Já existe um cliente com este e-mail.");
+        }
+
         var cliente = new Cliente(clienteNovo);
         clienteRepository.salva(cliente);
         log.info("[Finaliza] ClienteApplicationService - criaNovoCliente");
