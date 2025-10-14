f6126b6529081de6a56a10cb19f56ef2277bb544
Felipe Moraski <moraski060@gmail.com>
2025-10-14 20:21:34 -0300

    feat: Criação do método buscaPorId em ClienteRepository e em ClienteRepositoryMySQL

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/application/repository/ClienteRepository.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/repository/ClienteRepository.java
@@ -3,8 +3,10 @@ package com.Moraski.DeliveryAPI.Cliente.application.repository;
 import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ClienteRepository {
     Cliente salva(Cliente cliente);
     Optional<Cliente> buscaPorEmail(String email);
+    Cliente buscaPorId(UUID idCliente);
 }
--- src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteRepositoryMySQL.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteRepositoryMySQL.java
@@ -2,11 +2,14 @@ package com.Moraski.DeliveryAPI.Cliente.infra;
 
 import com.Moraski.DeliveryAPI.Cliente.application.repository.ClienteRepository;
 import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
+import com.Moraski.DeliveryAPI.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,6 +31,15 @@ public class ClienteRepositoryMySQL implements ClienteRepository {
         log.info("[Inicia] ClienteRepositoryMySQL - buscaPorEmail");
         Optional<Cliente> cliente = clienteMySQLSpringRepository.findByEmail(email);
         log.info("[Finaliza] ClienteRepositoryMySQL - buscaPorEmail");
+        return cliente;
+    }
+
+    @Override
+    public Cliente buscaPorId(UUID idCliente) {
+        log.info("[Inicia] ClienteRepositoryMySQL - buscaPorId");
+        Cliente cliente = clienteMySQLSpringRepository.findById(idCliente)
+                        .orElseThrow(()-> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
+        log.info("[Finaliza] ClienteRepositoryMySQL - buscaPorId");
         return cliente;
     }
 }
