464cbeb2a9007c046104a97d921fdbb157d63063
Felipe Moraski <moraski060@gmail.com>
2025-10-14 16:56:13 -0300

    feat: Criação do método findByEmail em ClienteMySQLSpringRepository

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteMySQLSpringRepository.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteMySQLSpringRepository.java
@@ -3,7 +3,9 @@ package com.Moraski.DeliveryAPI.Cliente.infra;
 import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClienteMySQLSpringRepository extends JpaRepository<Cliente, UUID> {
+    Optional<Cliente> findByEmail(String email);
 }
