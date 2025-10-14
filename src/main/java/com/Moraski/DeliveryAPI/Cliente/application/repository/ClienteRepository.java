7fac9f40a9d11bd240b87463c892f4d43f3f8e8e
Felipe Moraski <moraski060@gmail.com>
2025-10-14 16:19:10 -0300

    feat: Criação do ClienteRepositoryMySQL em Cliente.infra

---
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/repository/ClienteRepository.java
@@ -0,0 +1,4 @@
+package com.Moraski.DeliveryAPI.Cliente.application.repository;
+
+public interface ClienteRepository {
+}
---
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/infra/ClienteRepositoryMySQL.java
@@ -0,0 +1,6 @@
+package com.Moraski.DeliveryAPI.Cliente.infra;
+
+import com.Moraski.DeliveryAPI.Cliente.application.repository.ClienteRepository;
+
+public class ClienteRepositoryMySQL implements ClienteRepository {
+}
