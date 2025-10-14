0aa24b2f1487258755d6858f0928c5f0d8c90ec6
Felipe Moraski <moraski060@gmail.com>
2025-10-14 16:15:44 -0300

    feat: Criação do ClienteController em Cliente.application.api

---
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/api/ClienteController.java
@@ -0,0 +1,24 @@
+package com.Moraski.DeliveryAPI.Cliente.application.api;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@Log4j2
+@RequiredArgsConstructor
+public class ClienteController implements ClienteAPI{
+
+    private final ClienteService clienteService;
+
+    @Override
+    public ClienteResponse postNovoCliente(@Valid ClienteNovoRequest cliente) {
+        log.info("[Inicia] ClienteController - postNovoCliente");
+        ClienteResponse clienteResponse = clienteService.criarNovoCliente(cliente);
+        log.info("[Finaliza] ClienteController - postNovoCliente");
+        return clienteResponse;
+    }
+}
