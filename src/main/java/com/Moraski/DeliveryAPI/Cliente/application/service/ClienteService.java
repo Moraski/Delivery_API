8c645d8c4f9f6282093b5ea012c3c19dd678908d
Felipe Moraski <moraski060@gmail.com>
2025-10-15 00:16:20 -0300

    feat: Criação do método editaCliente em ClienteService e ClienteApplicationService

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/application/service/ClienteService.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/service/ClienteService.java
@@ -2,6 +2,7 @@ package com.Moraski.DeliveryAPI.Cliente.application.service;
 
 import com.Moraski.DeliveryAPI.Cliente.application.api.ClienteNovoRequest;
 import com.Moraski.DeliveryAPI.Cliente.application.api.ClienteResponse;
+import com.Moraski.DeliveryAPI.Cliente.application.api.EditaClienteRequest;
 import jakarta.validation.Valid;
 
 import java.util.UUID;
@@ -9,4 +10,5 @@ import java.util.UUID;
 public interface ClienteService {
     ClienteResponse criarNovoCliente(@Valid ClienteNovoRequest cliente);
     ClienteResponse buscarClientePorId(UUID idCliente);
+    ClienteResponse editaCliente(UUID idCliente, EditaClienteRequest editaClienteRequest);
 }
