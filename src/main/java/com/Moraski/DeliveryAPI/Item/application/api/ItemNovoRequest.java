b9a62dc4387a048dbf453e7636ed610a9b897a32
Felipe Moraski <moraski060@gmail.com>
2025-10-17 17:49:39 -0300

    fix: Ajustes em ItemNovoRequest

--- src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemNovoRequest.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemNovoRequest.java
@@ -1,5 +1,7 @@
 package com.Moraski.DeliveryAPI.Item.application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 import java.util.UUID;
@@ -8,7 +10,16 @@ import java.util.UUID;
 public class ItemNovoRequest {
 
     UUID idItem;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 100, message = "Nome não pode ultrapassar 100 caracteres")
     String produto;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 100, message = "Nome não pode ultrapassar 100 caracteres")
     Double valorUniterio;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 100, message = "Nome não pode ultrapassar 100 caracteres")
     String descricao;
 }
