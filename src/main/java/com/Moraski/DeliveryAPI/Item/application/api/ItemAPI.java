321ddc38fba4ec892ae063ccb39dc4a593b7b9fa
Felipe Moraski <moraski060@gmail.com>
2025-10-18 11:36:04 -0300

    feat: cria o método postNovoItem em ItemAPI e em ItemController

--- src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemAPI.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemAPI.java
@@ -1,7 +1,16 @@
 package com.Moraski.DeliveryAPI.Item.application.api;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequestMapping(value = "/public/v1/item")
 public interface ItemAPI {
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ItemResponse postNovoItem(@RequestBody @Valid ItemNovoRequest item);
 }
--- src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemController.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/api/ItemController.java
@@ -1,6 +1,7 @@
 package com.Moraski.DeliveryAPI.Item.application.api;
 
 
+import com.Moraski.DeliveryAPI.Item.application.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.validation.annotation.Validated;
@@ -11,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 @RequiredArgsConstructor
 public class ItemController implements ItemAPI{
+
+    private  final ItemService itemservece;
+
+    @Override
+    public ItemResponse postNovoItem(ItemNovoRequest item) {
+        log.info("[Inicia] ItemController - postNovoItem");
+        ItemResponse response = itemservece.criarNovoItem(item);
+        log.info("[Finaliza] ItemController - postNovoItem");
+        return response;
+    }
 }
