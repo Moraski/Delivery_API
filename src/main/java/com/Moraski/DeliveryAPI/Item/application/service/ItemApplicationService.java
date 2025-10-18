ccb7a9233114dc19854ffdb28ef83d890f35e04c
Felipe Moraski <moraski060@gmail.com>
2025-10-18 11:41:54 -0300

    feat: cria o método criarNovoItem em ItemService e em ItemApplicationService

--- src/main/java/com/Moraski/DeliveryAPI/Item/application/service/ItemApplicationService.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/service/ItemApplicationService.java
@@ -1,5 +1,9 @@
 package com.Moraski.DeliveryAPI.Item.application.service;
 
+import com.Moraski.DeliveryAPI.Item.application.api.ItemNovoRequest;
+import com.Moraski.DeliveryAPI.Item.application.api.ItemResponse;
+import com.Moraski.DeliveryAPI.Item.application.repository.ItemRepository;
+import com.Moraski.DeliveryAPI.Item.domain.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ItemApplicationService implements ItemService{
+
+    private final ItemRepository itemRepository;
+
+    @Override
+    public ItemResponse criarNovoItem(ItemNovoRequest itemNovo) {
+        log.info("[Inicia] ClienteApplicationService - criaNovoCliente");
+        var item = new Item(itemNovo);
+        itemRepository.salva(item);
+        log.info("[Finaliza] ClienteApplicationService - criaNovoCliente");
+        return new ItemResponse(item);
+    }
 }
