0a4f48a41fc49193bf627fd628107eb76f4be751
Felipe Moraski <moraski060@gmail.com>
2025-10-20 18:27:38 -0300

    feat: cria o método bucaPorId em ItemRepository e em ItemRepositoryMySQL

--- src/main/java/com/Moraski/DeliveryAPI/Item/application/repository/ItemRepository.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/repository/ItemRepository.java
@@ -2,6 +2,9 @@ package com.Moraski.DeliveryAPI.Item.application.repository;
 
 import com.Moraski.DeliveryAPI.Item.domain.Item;
 
+import java.util.UUID;
+
 public interface ItemRepository {
     Item salva(Item item);
+    Item bucaPorId(UUID idItem);
 }
