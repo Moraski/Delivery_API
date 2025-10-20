3dd280677528b64c047dbdc671a4e5e37aba40b1
Felipe Moraski <moraski060@gmail.com>
2025-10-20 18:16:13 -0300

    feat: cria o método buscarItemPorId em ItemApplicationService e ItemService

--- src/main/java/com/Moraski/DeliveryAPI/Item/application/service/ItemService.java
+++ src/main/java/com/Moraski/DeliveryAPI/Item/application/service/ItemService.java
@@ -3,6 +3,9 @@ package com.Moraski.DeliveryAPI.Item.application.service;
 import com.Moraski.DeliveryAPI.Item.application.api.ItemNovoRequest;
 import com.Moraski.DeliveryAPI.Item.application.api.ItemResponse;
 
+import java.util.UUID;
+
 public interface ItemService {
     ItemResponse criarNovoItem(ItemNovoRequest itemNovo);
+    ItemResponse buscarItemPorId(UUID idItem);
 }
