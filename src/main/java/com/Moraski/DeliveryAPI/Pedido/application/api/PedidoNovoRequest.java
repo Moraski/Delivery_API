36eeb7ec23f87bcc07c5de8c30be3c9a669d3d79
Felipe Moraski <moraski060@gmail.com>
2025-10-21 19:24:35 -0300

    fix: Corrige o PedidoNovoRequest

--- src/main/java/com/Moraski/DeliveryAPI/Pedido/application/api/PedidoNovoRequest.java
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/application/api/PedidoNovoRequest.java
@@ -2,6 +2,11 @@ package com.Moraski.DeliveryAPI.Pedido.application.api;
 
 import com.Moraski.DeliveryAPI.ItensDoPedido.application.api.ItensDoPedidoRequest;
 import com.Moraski.DeliveryAPI.Pedido.domain.StatusPedido;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 import java.util.List;
@@ -10,8 +15,17 @@ import java.util.UUID;
 @Value
 public class PedidoNovoRequest {
 
+    @NotNull(message = "idCliente é obrigatório")
     UUID idCliente;
+
+    @NotBlank(message = "Endereço de entrega é obrigatório")
+    @Size(max = 100, message = "Endereço de entrega não pode ultrapassar 100 caracteres")
     String enderecoEntrega;
+
+    @NotNull(message = "O status do pedido é obrigatório")
     StatusPedido statusPedido;
+
+    @Valid
+    @NotEmpty(message = "O pedido deve conter pelo menos um item")
     List<ItensDoPedidoRequest> itensDoPedido;
 }
