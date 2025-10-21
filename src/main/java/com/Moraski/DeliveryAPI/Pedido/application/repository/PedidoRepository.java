bcdd24e0d247c227bdf28cc942a43f767cc230d1
Felipe Moraski <moraski060@gmail.com>
2025-10-21 19:11:25 -0300

    feat: cria as classes PedidoRepository, PedidoMySQLSpringRepository e PedidoRepository e faz a pré configuração

---
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/application/repository/PedidoRepository.java
@@ -0,0 +1,4 @@
+package com.Moraski.DeliveryAPI.Pedido.application.repository;
+
+public interface PedidoRepository {
+}
---
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/infra/PedidoRepositoryMySQL.java
@@ -0,0 +1,12 @@
+package com.Moraski.DeliveryAPI.Pedido.infra;
+
+import com.Moraski.DeliveryAPI.Pedido.application.repository.PedidoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class PedidoRepositoryMySQL implements PedidoRepository {
+}
