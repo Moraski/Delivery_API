d6f677e2e410ab005cb625b810f1d9bc9975c509
Felipe Moraski <moraski060@gmail.com>
2025-10-21 23:05:15 -0300

    feat: cria o método buscarPedidoPorId em PedidoService e PedidoApplicationService

--- src/main/java/com/Moraski/DeliveryAPI/Pedido/application/service/PedidoService.java
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/application/service/PedidoService.java
@@ -3,6 +3,9 @@ package com.Moraski.DeliveryAPI.Pedido.application.service;
 import com.Moraski.DeliveryAPI.Pedido.application.api.PedidoNovoRequest;
 import com.Moraski.DeliveryAPI.Pedido.application.api.PedidoResponse;
 
+import java.util.UUID;
+
 public interface PedidoService {
     PedidoResponse criarNovoPedido(PedidoNovoRequest request);
+    PedidoResponse buscarPedidoPorId(UUID idPedido);
 }
