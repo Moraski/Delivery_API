c7dc0e60121941850e60897272b58d3fb4a9b20a
Felipe Moraski <moraski060@gmail.com>
2025-10-21 22:49:46 -0300

    feat: cria o método salva em PedidoRepository e PedidoRepositoryMySQL e o método bucaPorIdCliente em PedidoMySQLSpringRepository

--- src/main/java/com/Moraski/DeliveryAPI/Pedido/application/repository/PedidoRepository.java
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/application/repository/PedidoRepository.java
@@ -1,4 +1,7 @@
 package com.Moraski.DeliveryAPI.Pedido.application.repository;
 
+import com.Moraski.DeliveryAPI.Pedido.domain.Pedido;
+
 public interface PedidoRepository {
+    Pedido salva(Pedido pedido);
 }
--- src/main/java/com/Moraski/DeliveryAPI/Pedido/infra/PedidoRepositoryMySQL.java
+++ src/main/java/com/Moraski/DeliveryAPI/Pedido/infra/PedidoRepositoryMySQL.java
@@ -1,6 +1,7 @@
 package com.Moraski.DeliveryAPI.Pedido.infra;
 
 import com.Moraski.DeliveryAPI.Pedido.application.repository.PedidoRepository;
+import com.Moraski.DeliveryAPI.Pedido.domain.Pedido;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,14 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 @Slf4j
 public class PedidoRepositoryMySQL implements PedidoRepository {
+
+    private final PedidoMySQLSpringRepository pedidoMySQLSpringRepository;
+
+    @Override
+    public Pedido salva(Pedido pedido) {
+        log.info("[Inicia] PedidoRepositoryMySQL - salva");
+        Pedido novoPedido = pedidoMySQLSpringRepository.save(pedido);
+        log.info("[finaliza] PedidoRepositoryMySQL - salva");
+        return novoPedido;
+    }
 }
