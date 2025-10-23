2084988d778812bd8b346cc60ddf5eb1429c7683
Felipe Moraski <moraski060@gmail.com>
2025-10-23 16:48:45 -0300

    feat: atualizar APIs com annotations do Swagger/OpenAPI para documentação

--- src/main/java/com/Moraski/DeliveryAPI/Cliente/application/api/ClienteAPI.java
+++ src/main/java/com/Moraski/DeliveryAPI/Cliente/application/api/ClienteAPI.java
@@ -1,28 +1,34 @@
 package com.Moraski.DeliveryAPI.Cliente.application.api;
 
-import com.Moraski.DeliveryAPI.Cliente.domain.Cliente;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@Tag(name = "Cliente", description = "Endpoints de gestão de clientes")
 @RequestMapping(value = "/public/v1/cliente")
 
 
 public interface ClienteAPI {
+    @Operation(summary = "Cria um novo cliente")
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     ClienteResponse postNovoCliente(@RequestBody @Valid ClienteNovoRequest cliente);
 
+    @Operation(summary = "Busca um cliente pelo ID")
     @GetMapping(value = "/{idCliente}")
     @ResponseStatus(code = HttpStatus.OK)
     ClienteResponse getClientePorId(@PathVariable UUID idCliente);
 
+    @Operation(summary = "Altera os dados de um cliente")
     @PutMapping(value = "/{idCliente}")
     @ResponseStatus(code = HttpStatus.OK)
     ClienteResponse editaCliente(@PathVariable UUID idCliente, @RequestBody @Valid EditaClienteRequest editaClienteRequest);
 
+    @Operation(summary = "Deleta um cliente existente")
     @DeleteMapping(value = "/{idCliente}/delete")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void deletaCliente(@PathVariable UUID idCliente);
